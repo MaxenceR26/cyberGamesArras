cb9d5c84daba1670e82448fb64d29c7934e26c74
MaxenceR26 <maxencedu62_@outlook.fr>
2025-01-20 14:44:55 +0100

    feat: :construction: login
    Setting up login form

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,10 +3,12 @@
 namespace App\DataFixtures;
 
 use App\Entity\Token;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 use Faker\Generator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
@@ -17,15 +19,19 @@ class AppFixtures extends Fixture
 
     public function __construct()
     {
-        $this->faker = Factory::create('en_US');
+        $this->faker = Factory::create('fr_FR');
     }
 
     public function load(ObjectManager $manager): void
     {
         for ($i=0;$i <= 20; $i++) { 
-            $machines = new Token();
-            $machines->setToken($this->faker->phoneNumberWithExtension());
-            $manager->persist($machines);
+            $user = new User();
+            $user->setFullName($this->faker->name())
+                ->setPseudo(mt_rand(0, 1) === 1 ? $this->faker->firstName() : null)
+                ->setEmail($this->faker->email())
+                ->setRoles(['ROLE_USER'])
+                ->setPlainPassword('password');
+            $manager->persist($user);
         }
 
         $manager->flush();
