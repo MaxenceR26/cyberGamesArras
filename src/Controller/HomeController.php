01ee84799fdbe936bff053a5ae05717f57eef81b
MaxenceR26 <maxencedu62_@outlook.fr>
2025-02-05 16:04:40 +0100

    feat: :page_facing_up: RGPD
    Adding RGPD compliance

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -23,4 +23,28 @@ class HomeController extends AbstractController
     {
         return $this->render('pages/about.html.twig');
     }
+
+    #[Route('/metions-legales', name:'conform.legal', methods: ['GET'])]
+    public function legal(): Response
+    {
+        return $this->render('legal.html.twig');
+    }
+
+    #[Route('/cgu', name:'conform.cgu', methods: ['GET'])]
+    public function cgu(): Response
+    {
+        return $this->render('cgu.html.twig');
+    }
+
+    #[Route('/cgv', name:'conform.cgv', methods: ['GET'])]
+    public function cgv(): Response
+    {
+        return $this->render('cgv.html.twig');
+    }
+
+    #[Route('/politique-confidentialite', name:'conform.polconf', methods: ['GET'])]
+    public function polConf(): Response
+    {
+        return $this->render('politique_confidentialite.html.twig');
+    }
 }
--- src/Repository/TokenRepository.php
+++ src/Repository/TokenRepository.php
@@ -21,28 +21,13 @@ class TokenRepository extends ServiceEntityRepository
         parent::__construct($registry, Token::class);
     }
 
-//    /**
-//     * @return Token[] Returns an array of Token objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('t.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Token
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function findExpiredTokens()
+    {
+        // Requête pour récupérer les tokens expirés
+        return $this->createQueryBuilder('t')
+            ->where('t.hours < :nowMinusTwoHours')
+            ->setParameter('nowMinusTwoHours', new \DateTime('-2 hours'))
+            ->getQuery()
+            ->getResult();
+    }
 }
